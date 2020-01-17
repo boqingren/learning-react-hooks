7c5eb3cf1031e52685034548d1702010b07db12f
boqingren <boqingren@hotmail.com>
2020-01-17 17:10:08 +0800

    add styled-components/react-redux/immutable

--- src/pages/MyUseState/index.js
+++ src/pages/MyUseState/index.js
@@ -1,7 +1,30 @@
-import * as React from "react";
+import React, { Fragment, useState } from "react";
 
-const MyUseState = () => {
-  return <div>MyUseState ...</div>;
-};
+const Title = React.memo(props => {
+  return (
+    <div style={{ textAlign: "left" }}>
+      {props.text}
+    </div>
+  );
+});
+
+// const [count, setCount] = useState(0);
+
+const Counter = React.memo(props => {
+  return (
+    <div style={{ textAlign: "left" }}>
+      {props.text}
+    </div>
+  );
+});
+
+const MyUseState = React.memo(props => {
+
+  return (
+    <Fragment>
+      <Title text="MyUseState" />
+    </Fragment>
+  );
+});
 
 export default MyUseState;
